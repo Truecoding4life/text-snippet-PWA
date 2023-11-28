a613064ea0ce3ad3ea052a7cb7f147c10773db23
Master <nghiemxthai@gmail.com>
2023-11-27 18:15:34 -0800

    added pseudo code

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,6 +2,7 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
+
 window.addEventListener('beforeinstallprompt', (event) => {});
 
 // TODO: Implement a click event handler on the `butInstall` element
@@ -9,3 +10,6 @@ butInstall.addEventListener('click', async () => {});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {});
+
+
+//Deferred prompt
