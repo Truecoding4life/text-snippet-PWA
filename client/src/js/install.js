7cbcd5465a35ca6e2f772007f10e179b70b85cf9
Master <nghiemxthai@gmail.com>
2024-02-20 14:55:38 -0800

    fix default text display

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,14 +2,12 @@ const butInstall = document.getElementById('buttonInstall');
 
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 
 window.addEventListener('beforeinstallprompt', (event) => { 
     window.deferredPrompt = event;
     butInstall.classList.toggle('hidden', false);
 })
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
 
@@ -24,10 +22,8 @@ butInstall.addEventListener('click', async () => {
     butInstall.classList.toggle('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     window.deferredPrompt = null;
 });
 
 
-//Deferred prompt
